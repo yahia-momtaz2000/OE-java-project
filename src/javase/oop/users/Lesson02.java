3e8e38bb665a5d3f086309e7eb3da2f80e8250c2
yahia-momtaz2000 <senior.steps.info@gmail.com>
2024-01-08 06:12:02 +0200

    Adding Lesson02

---
+++ src/javase/oop/users/Lesson02.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javase.oop.users;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Lesson02 {
+    public static void main(String[] args) {
+        System.out.println("Lesson02");
+    }
+}
